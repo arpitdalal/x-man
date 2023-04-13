362acabcf9884405cc629c920df5d25122c9931c
arpitdalal <arpitdalalm@gmail.com>
2023-04-13 15:40:36 -0400

    🩹 fix scroll issue on ios on empty page

--- public/entry.worker.js
+++ public/entry.worker.js
@@ -1,8 +1,27 @@
+// node_modules/@remix-run/server-runtime/dist/esm/responses.js
+var json = (data, init = {}) => {
+  let responseInit = typeof init === "number" ? {
+    status: init
+  } : init;
+  let headers = new Headers(responseInit.headers);
+  if (!headers.has("Content-Type")) {
+    headers.set("Content-Type", "application/json; charset=utf-8");
+  }
+  return new Response(JSON.stringify(data), {
+    ...responseInit,
+    headers
+  });
+};
+
 // app/entry.worker.ts
+var STATIC_ASSETS = ["/build/", "/icons/", "/"];
+var ASSET_CACHE = "asset-cache";
 var DATA_CACHE = "data-cache";
 var DOCUMENT_CACHE = "document-cache";
 function debug(...messages) {
-  console.log(...messages);
+  if (true) {
+    console.debug(...messages);
+  }
 }
 async function handleInstall(event) {
   debug("Service worker installed");
@@ -52,6 +71,73 @@ async function handleMessage(event) {
   }
   await Promise.all(cachePromises.values());
 }
+async function handleFetch(event) {
+  const url = new URL(event.request.url);
+  if (isAssetRequest(event.request)) {
+    const cached = await caches.match(event.request, {
+      cacheName: ASSET_CACHE,
+      ignoreVary: true,
+      ignoreSearch: true
+    });
+    if (cached) {
+      debug("Serving asset from cache", url.pathname);
+      return cached;
+    }
+    debug("Serving asset from network", url.pathname);
+    const response = await fetch(event.request);
+    if (response.status === 200) {
+      const cache = await caches.open(ASSET_CACHE);
+      await cache.put(event.request, response.clone());
+    }
+    return response;
+  }
+  if (isLoaderRequest(event.request)) {
+    try {
+      debug("Serving data from network", url.pathname + url.search);
+      const response = await fetch(event.request.clone());
+      const cache = await caches.open(DATA_CACHE);
+      await cache.put(event.request, response.clone());
+      return response;
+    } catch (error) {
+      debug(
+        "Serving data from network failed, falling back to cache",
+        url.pathname + url.search
+      );
+      const response = await caches.match(event.request);
+      if (response) {
+        response.headers.set("X-Remix-Worker", "yes");
+        return response;
+      }
+      return json(
+        { message: "Network Error" },
+        {
+          status: 500,
+          headers: { "X-Remix-Catch": "yes", "X-Remix-Worker": "yes" }
+        }
+      );
+    }
+  }
+  if (isDocumentGetRequest(event.request)) {
+    try {
+      debug("Serving document from network", url.pathname);
+      const response = await fetch(event.request);
+      const cache = await caches.open(DOCUMENT_CACHE);
+      await cache.put(event.request, response.clone());
+      return response;
+    } catch (error) {
+      debug(
+        "Serving document from network failed, falling back to cache",
+        url.pathname
+      );
+      const response = await caches.match(event.request);
+      if (response) {
+        return response;
+      }
+      throw error;
+    }
+  }
+  return fetch(event.request.clone());
+}
 var handlePush = async (event) => {
   const data = JSON.parse(event == null ? void 0 : event.data.text());
   const title = data.title ? data.title : "Remix PWA";
@@ -67,6 +153,19 @@ var handlePush = async (event) => {
     ...options
   });
 };
+function isMethod(request, methods) {
+  return methods.includes(request.method.toLowerCase());
+}
+function isAssetRequest(request) {
+  return isMethod(request, ["get"]) && STATIC_ASSETS.some((publicPath) => request.url.startsWith(publicPath));
+}
+function isLoaderRequest(request) {
+  const url = new URL(request.url);
+  return isMethod(request, ["get"]) && url.searchParams.get("_data");
+}
+function isDocumentGetRequest(request) {
+  return isMethod(request, ["get"]) && request.mode === "navigate";
+}
 self.addEventListener("install", (event) => {
   event.waitUntil(handleInstall(event).then(() => self.skipWaiting()));
 });
@@ -79,3 +178,22 @@ self.addEventListener("message", (event) => {
 self.addEventListener("push", (event) => {
   event.waitUntil(handlePush(event));
 });
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    (async () => {
+      const result = {};
+      try {
+        result.response = await handleFetch(event);
+      } catch (error) {
+        result.error = error;
+      }
+      return appHandleFetch(event, result);
+    })()
+  );
+});
+async function appHandleFetch(event, {
+  error,
+  response
+}) {
+  return response;
+}
